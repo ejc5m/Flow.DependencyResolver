1d7114d2e0c70b0c67f0b90d7379fcf697aa49e7
ejc5m <106887035+ejc5m@users.noreply.github.com>
2026-06-08 18:17:38 -0400

    Made it so if duplicates are detected, the first instance of the key also fails

--- src/Flow.DependencyResolver/Internal/Graph/GraphBuilder.cs
+++ src/Flow.DependencyResolver/Internal/Graph/GraphBuilder.cs
@@ -8,12 +8,19 @@ internal static class GraphBuilder
     internal static Graph<TKey> Build<TKey>(IReadOnlyCollection<DependencyNode<TKey>> nodes, FailureCollection<TKey> failureCollection) where TKey : notnull
     {
         Graph<TKey> graph = new();
+        HashSet<TKey> duplicates = [];
 
         foreach (var node in nodes)
         {
             if (graph.Forward.ContainsKey(node.Key))
             {
+                duplicates.Add(node.Key);
                 failureCollection.AddGlobalFailure(new DuplicateKeyFailure<TKey>(node.Key));
+
+                //Remove this key from the graph or else the topological sorter will think it's valid
+                graph.Forward.Remove(node.Key);
+                graph.Reverse.Remove(node.Key);
+
                 continue;
             }
 
@@ -21,12 +28,18 @@ internal static class GraphBuilder
             graph.Reverse[node.Key] = [];
         }
 
-        HashSet<TKey> knownKeys = nodes.Select(x => x.Key).ToHashSet();
+        HashSet<TKey> knownKeys = nodes.Select(node => node.Key).Where(key => !duplicates.Contains(key)).ToHashSet();
 
         foreach (var node in nodes)
         {
             foreach (var dependency in node.Dependencies)
             {
+                if (duplicates.Contains(dependency.Key))
+                {
+                    failureCollection.AddFailureReason(node.Key, new InvalidDependencyFailure<TKey>(dependency.Key));
+                    continue;
+                }
+
                 if (!knownKeys.Contains(dependency.Key))
                 {
                     if (dependency.IsOptional)
